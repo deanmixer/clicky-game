efbd2e12e9a2b2c07a60ce889c5925db1455d8f5
deanmixer <deanmixer@users.noreply.github.com>
2019-04-06 22:11:10 -0400

    initial commit

---
+++ build/precache-manifest.f698ceff6100718d07b1d729aa36d35c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "f23a2719c81ee72506e7",
+    "url": "/static/js/main.530edcc2.chunk.js"
+  },
+  {
+    "revision": "eb5d03ae7c9a30028659",
+    "url": "/static/js/2.4e81ba5b.chunk.js"
+  },
+  {
+    "revision": "f23a2719c81ee72506e7",
+    "url": "/static/css/main.9de3793f.chunk.css"
+  },
+  {
+    "revision": "286e55b03b15e4b0cea40c6aebebdefd",
+    "url": "/index.html"
+  }
+];
